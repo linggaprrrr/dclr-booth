d6d6047550933832325d293454c2ccd30fb53897
Rangga Adhitya <deneuv3.4@gmail.com>
2025-03-08 23:50:52 +0700

    feat: add upload endpoint

--- src/server/routes/pictures/index.ts
+++ src/server/routes/pictures/index.ts
@@ -1,11 +1,12 @@
 import listPictures from "../../controllers/listPicture";
 import takePicture from "../../controllers/takePicture";
-
+import uploadPictures from "../../controllers/uploadPictures";
 import { Router } from "express";
 
 export const pictureRoutes = (router: Router) => {
   router.get('/capture', takePicture);
   router.get('/list', listPictures);
+  router.post('/upload', uploadPictures);
 }
 
 export default pictureRoutes;
